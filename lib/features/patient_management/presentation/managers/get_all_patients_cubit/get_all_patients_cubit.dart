a848265203927468f447b779b7a0c7d6450aca0d
mohamedrafat <mtag66870@gmail.com>
2026-06-22 21:13:07 +0300

    implement search feature

--- lib/features/patient_management/presentation/managers/get_all_patients_cubit/get_all_patients_cubit.dart
+++ lib/features/patient_management/presentation/managers/get_all_patients_cubit/get_all_patients_cubit.dart
@@ -4,11 +4,14 @@ import 'package:bloc/bloc.dart';
 import 'package:medistats/features/patient_management/data/models/patient_model.dart';
 import 'package:medistats/features/patient_management/data/repos/patient_repo.dart';
 import 'package:meta/meta.dart';
+
 part 'get_all_patients_state.dart';
 
 class GetAllPatientsCubit extends Cubit<GetAllPatientsState> {
   final PatientRepo patientRepo;
   StreamSubscription? _patientsSubscription;
+
+  List<PatientModel> _allPatients = [];
 
   GetAllPatientsCubit(this.patientRepo) : super(GetAllPatientsInitial());
 
@@ -18,11 +21,26 @@ class GetAllPatientsCubit extends Cubit<GetAllPatientsState> {
     _patientsSubscription?.cancel();
 
     _patientsSubscription = patientRepo.getAllPatients().listen((result) {
-      result.fold(
-            (failure) => emit(GetAllPatientsError(failure.message)),
-            (patients) => emit(GetAllPatientsSuccess(patients)),
-      );
+      result.fold((failure) => emit(GetAllPatientsError(failure.message)), (
+        patients,
+      ) {
+        _allPatients = patients;
+        emit(GetAllPatientsSuccess(patients));
+      });
     });
+  }
+
+
+  void searchPatients(String query) {
+    if (query.isEmpty) {
+      emit(GetAllPatientsSuccess(_allPatients));
+    } else {
+      final filteredPatients = _allPatients.where((patient) {
+        return patient.name.toLowerCase().contains(query.toLowerCase());
+      }).toList();
+
+      emit(GetAllPatientsSuccess(filteredPatients));
+    }
   }
 
   @override
--- lib/features/patient_management/presentation/views/widgets/search_field.dart
+++ lib/features/patient_management/presentation/views/widgets/search_field.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medistats/features/patient_management/presentation/managers/add_patient_cubit/add_patient_cubit.dart';
+import 'package:medistats/features/patient_management/presentation/managers/get_all_patients_cubit/get_all_patients_cubit.dart';
 
 class SearchField extends StatelessWidget {
   const SearchField({super.key});
@@ -12,13 +15,22 @@ class SearchField extends StatelessWidget {
         color: const Color(0xFFD9DEE1),
         borderRadius: BorderRadius.circular(24),
       ),
-      child: const Row(
+      child: Row(
         children: [
-          Icon(Icons.search, color: Colors.black54),
-          SizedBox(width: 8),
-          Text(
-            'Search patients...',
-            style: TextStyle(color: Colors.black54, fontSize: 16),
+          const Icon(Icons.search, color: Colors.black54),
+          const SizedBox(width: 8),
+          Expanded(
+            child: TextField(
+              onChanged: (value) {
+                context.read<GetAllPatientsCubit>().searchPatients(value);
+              },
+              decoration: const InputDecoration(
+                hintText: 'Search patients...',
+                hintStyle: TextStyle(color: Colors.black54, fontSize: 16),
+                border: InputBorder.none,
+                isDense: true,
+              ),
+            ),
           ),
         ],
       ),
