9bd7ab9870cae81df54bdfbf5c68c36a3d6c7513
mohamedrafat <mtag66870@gmail.com>
2026-06-22 06:51:01 +0300

    create firestore service

---
+++ lib/core/services/firestore_service.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FireStoreService {
+  FirebaseFirestore _fireStore = FirebaseFirestore.instance;
+
+  Future<String> addData(
+    String collectionName,
+    Map<String, dynamic> data,
+    String? uid,
+  ) async {
+    if (uid != null && uid.isNotEmpty) {
+      await _fireStore.collection(collectionName).doc(uid).set(data);
+      return uid;
+    } else {
+      DocumentReference documentReference = await _fireStore.collection(collectionName).add(data);
+      return documentReference.id;
+    }
+  }
+}
